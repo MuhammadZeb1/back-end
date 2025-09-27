0e8ffd63370e4340accf2bd5f309e92a2c20ea7b
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-09-27 06:16:07 -0700

    revise the path module

---
+++ server/built-in-module/path.js
@@ -0,0 +1,37 @@
+const path = require("path")
+
+// console.log(`__dirname ${__dirname}`)// __dirname C:\Users\M ZEB\Desktop\back-end\server\built-in-module(only with folder) 
+
+// console.log(`__filename ${__filename}`) //__filename C:\Users\M ZEB\Desktop\back-end\server\built-in-module\path.js(with file)
+
+// school folder/student/data.js  
+
+
+const pathFile = path.join("folder","student","data.txt")//folder\student\data.txt
+
+// console.log(pathFile)
+
+const parseFile = path.parse(pathFile)
+//{  root: '',  dir: 'folder\\student',  base: 'data.txt',  ext: '.txt',  name: 'data'}
+
+// console.log(parseFile)
+
+const rsolvedPath = path.resolve(pathFile)
+//C:\Users\M ZEB\Desktop\back-end\server\built-in-module\folder\student\data.txt
+// console.log("reesolvedPath",    rsolvedPath)
+  
+
+// const extansionPath = path.extname(pathFile)
+// extansionPath .txt
+
+// console.log("extansionPath",extansionPath)
+
+
+// const baseName =  path.basename(pathFile) //baseName data.txt
+
+// console.log("baseName", baseName)
+
+
+const dirName = path.dirname(pathFile) //dirname folder\student
+
+console.log("dirname",dirName)
