09b5f669737867e119e6396bf0255d74460bcde0
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-09-29 03:14:13 -0700

    review the req.query

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
-
 const app = express();
 
-app.get("/:id",(req, res, ) => {
-    res.send(`Hello from Express! ${req.params.id}`);
-})
+// Dynamic route with id and slug
+app.get("/:id-:slug", (req, res) => {
+    res.send(`Hello from Express! ID: ${req.params.id}, Slug: ${req.params.slug}`);
+});
+
+// Query params route
+app.get("/search", (req, res) => {
+    res.send(req.query)
+    // res.send(`Hello from Express! Query: ${JSON.stringify(req.query)}`);
+});
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
