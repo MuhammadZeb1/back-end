2b3350dbd49fbf4a68c4b21065eea09b8ed21beb
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-09-29 02:38:39 -0700

    complete the http module

--- server/http-server/index.js
+++ server/http-server/index.js
@@ -3,23 +3,42 @@ const fs = require("fs")
 
 const server = http.createServer((req, res) => {
 
-    const log = `${Date.now()} ${req.url} New request receved \n`;
+    if (req.url === "/favicon.ico") {
+        return res.end();
+    }
+    const log = `${Date.now()} ${req.method} ${req.url} New request receved \n`;
     console.log(log);
-    fs.appendFile("log.txt",log,(err,data)=>{
-   
+
+    fs.appendFile("log.txt", log, (err, data) => {
+
         console.log("Request received");
-        switch(req.url){
+        switch (req.url) {
             case "/":
-               console.log("Home page requested");
+                if (req.method === "GET") {
+                    res.end("your request is GET");
+                } else if (req.method === "POST") {
+                    res.end("your request is POST");
+                } else if (req.method === "PUT") {
+                    res.end("your request is PUT");
+                } else if (req.method === "PATCH") {
+                    res.end("your request is PATCH");
+                } else if (req.method === "DELETE") {
+                    res.end("your request is DELETE");
+                } else if (req.method === "OPTIONS") {
+                    res.end("your request is OPTIONS");
+                } else if (req.method === "HEAD") {
+                    res.writeHead(200);
+                    res.end(); // HEAD requests don’t return a body
+                } else {
+                    res.end(`Method ${req.method} not supported`);
+                }
                 break;
-            case "/favicon.ico":
-                console.log("Favicon requested");
-                break;
+
             default:
                 console.log("Unknown page requested");
+                res.end("Unknown page");
                 break;
         }
-        res.end('Hello, World!\n');
     })
 });
 
