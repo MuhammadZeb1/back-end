3f0774701758bd794e99d34acc66470245f86a43
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-09-29 06:03:00 -0700

    review the server side randing

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-
+app.use(express.static(path.join(__dirname, 'public')));
 // Dynamic route with id and slug
-app.get("/:id-:slug", (req, res) => {
-    res.send(`Hello from Express! ID: ${req.params.id}, Slug: ${req.params.slug}`);
-});
+// app.get("/:id-:slug", (req, res) => {
+//     res.send(`Hello from Express! ID: ${req.params.id}, Slug: ${req.params.slug}`);
+// });
 
 // Query params route
-app.get("/search", (req, res) => {
-    res.send(req.query)
-    // res.send(`Hello from Express! Query: ${JSON.stringify(req.query)}`);
+// app.get("/search", (req, res) => {
+//     res.send(req.query)
+//     // res.send(`Hello from Express! Query: ${JSON.stringify(req.query)}`);
+// });
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
 app.listen(3000, () => {
