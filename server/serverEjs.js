1d521d54caf1f14e06051a4b6184d44f2a0592e3
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-09-29 21:34:35 -0700

    learn the ejs and use that

--- server/serverEjs.js
+++ server/serverEjs.js
@@ -1,16 +1,23 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
 
 const app = express();
+// Express example
+app.use(express.static('public'));
 
-app.set("view engine", "ejs")
+
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// ✅ Route ek dafa hi likhna hai
 app.get("/", (req, res) => {
-    res.render("index")
-})
-
+    res.render("index", { 
+        title: "EJS Loop Example",
+        message: "My Skills List",
+        skills: ["HTML", "CSS", "JavaScript", "Node.js", "React"]
+    });
+});
 
 app.listen(3000, () => {
-    console.log("server is running on http://localhost:3000")
-})
+    console.log("server is running on http://localhost:3000");
+});
